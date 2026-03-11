4b51c21b5131b0adddfa6e752a32e43c1d755ed3
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-03-11 23:16:12 +0530

    the

--- button_animation/lib/neu_button.dart
+++ button_animation/lib/neu_button.dart
@@ -11,6 +11,24 @@ class NeuButton extends StatelessWidget{
         size:60,
         color:Colors.red[400],
       ),
+      decoration:BoxDecoration(
+        color:Colors.grey[300],
+        borderRadius:BorderRadius.circular(12),
+        boxShadow:[
+          BoxShadow(
+            color:Colors.grey.shade500,
+            offset:Offset(6, 6),
+            blurRadius:15,
+            spreadRadius:1, 
+          ),
+          BoxShadow(
+            color:Colors.white,
+            offset:Offset(-6, -6),
+            blurRadius:15,
+            spreadRadius:1, 
+          ),
+        ]
+      ),
     );
   }
 
