a4f9c95ab9e455fc46e145ec492f7db9490ac102
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-03-12 22:25:22 +0530

    Improved

--- button_animation/lib/main.dart
+++ button_animation/lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 import 'neu_button.dart';
-class HomePage extends StatefulWidget{
-  const HomePage({Key? key}):super(key:key);
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState()=>_HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
   @override
-  Widget build(BuildContext context){
-    return Scaffold();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Neumorphism only works if the background matches the button color
+      backgroundColor: Colors.grey[300], 
+      body: Center(
+        child: NeuButton(
+          onTap: () {
+            // This is where the magic happens when you click
+            print("Neumorphic Button Pressed!");
+          },
+        ),
+      ),
+    );
   }
 }
